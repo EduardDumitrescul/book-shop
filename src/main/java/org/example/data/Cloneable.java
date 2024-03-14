4a811f2a1da9e72033d728623d8e3eb24c812d73
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-03-14 15:37:50 +0200

    impl method to clone lists

--- src/main/java/org/example/data/Cloneable.java
+++ src/main/java/org/example/data/Cloneable.java
@@ -1,5 +1,16 @@
 package org.example.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Cloneable {
     Cloneable clone();
+
+    default List<Cloneable> cloneList(ArrayList<Cloneable> list) {
+        List<Cloneable> copy = new ArrayList<>();
+        for(Cloneable value: list) {
+            copy.add(value.clone());
+        }
+        return copy;
+    }
 }
